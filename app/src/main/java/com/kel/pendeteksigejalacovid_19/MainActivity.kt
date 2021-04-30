010576075e22dea71bfcd4972048fead0248109b
dzikrihf07 <dzikrihf@gmail.com>
2021-04-30 14:16:01 +0700

    Update Tampilan Selamat Datang

--- app/src/main/java/com/kel/pendeteksigejalacovid_19/MainActivity.kt
+++ app/src/main/java/com/kel/pendeteksigejalacovid_19/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.kel.pendeteksigejalacovid_19
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tblMasuk = findViewById<CardView>(R.id.btnMasuk)
+
+        tblMasuk.setOnClickListener() {
+            val go = Intent(this@MainActivity, DashboardActivity::class.java)
+            startActivity(go)
+        }
     }
 }
